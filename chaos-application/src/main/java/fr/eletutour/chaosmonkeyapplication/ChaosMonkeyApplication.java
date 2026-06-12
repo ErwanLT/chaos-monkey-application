2d7714d12f318b59815d6c0ea66737257e6cd777
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2026-06-12 11:10:49 +0200

    correction logs

--- chaos-application/src/main/java/fr/eletutour/chaosmonkeyapplication/ChaosMonkeyApplication.java
+++ chaos-application/src/main/java/fr/eletutour/chaosmonkeyapplication/ChaosMonkeyApplication.java
@@ -9,7 +9,7 @@ public class ChaosMonkeyApplication {
     public static void main(String[] args) {
         String profile = System.getProperty("spring.profiles.active", "default");
 
-        System.out.println("🎬 Starting Netflix-like Streaming Service with Chaos Monkey...");
+        System.out.println("🎬 Starting Streaming Service with Chaos Monkey...");
         System.out.println("🐵 Chaos Engineering Demo - Inspired by Netflix's Simian Army");
         System.out.println("🔧 Profil actif : " + profile);
         SpringApplication.run(ChaosMonkeyApplication.class, args);
